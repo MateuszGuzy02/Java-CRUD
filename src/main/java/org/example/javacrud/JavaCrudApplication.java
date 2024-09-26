dd9cddcc2c6fb68dc6d8090f2078850380ed7884
Mateusz Guzy <guzymateusz02@gmail.com>
2024-09-26 21:21:35 +0200

    Update

--- src/main/java/org/example/javacrud/JavaCrudApplication.java
+++ src/main/java/org/example/javacrud/JavaCrudApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaCrudApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JavaCrudApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCrudApplication.class, args);
+	}
 
 }
